2df95b5a11237b6c15bfa459176153534d525f08
Nico Nap <nico.nap@outlook.com>
2020-10-19 14:10:22 +0200

    Make validator for country and zipcode

--- scripts.js
+++ scripts.js
@@ -1,13 +1,13 @@
-const Email = function (id, errorSpan) {
-  let emailId = id;
-  let emailSpan = document.getElementById(errorSpan);
-  let input = document.getElementById(emailId);
+const Validator = function (id, errorSpan, type) {
+  let error = document.getElementById(errorSpan);
+  let input = document.getElementById(id);
+  let inputtype = type;
   let obj = this;
 
   this.init = function () {
     input.addEventListener("input", function () {
       if (input.validity.valid) {
-        emailSpan.innerHTML = "";
+        error.innerHTML = "";
       } else {
         obj.showError();
       }
@@ -15,13 +15,39 @@ const Email = function (id, errorSpan) {
   };
 
   this.showError = function () {
-    if (input.validity.typeMismatch) {
-      emailSpan.textContent = "Please enter a valid e-mail address.";
-    } else if (input.validity.valueMissing) {
-      emailSpan.textContent = "Please enter an e-mail address.";
+    if (inputtype == "email") {
+      if (input.validity.typeMismatch) {
+        error.textContent = "Please enter a valid e-mail address.";
+      } else if (input.validity.valueMissing) {
+        error.textContent = "Please enter an e-mail address.";
+      }
+    } else if (inputtype == "country") {
+      if (input.validity.tooLong) {
+        error.textContent = "Please enter less than 20 characters.";
+      } else if (input.validity.tooShort) {
+        error.textContent = "Please enter more than 3 characters.";
+      } else if (input.validity.valueMissing) {
+        error.textContent = "Please enter a country.";
+      }
+    } else if (inputtype == "zipcode") {
+      if (input.validity.patternMismatch) {
+        error.textContent = "Please enter a valid zipcode.";
+      } else if (input.validity.valueMissing) {
+        error.textContent = "Please enter a zipcode.";
+      } else if (input.validity.tooLong) {
+        error.textContent = "Please enter less than 10 characters.";
+      } else if (input.validity.tooShort) {
+        error.textContent = "Please enter more than 3 characters.";
+      }
     }
   };
 };
 
-let emailinput = new Email("email", "emailspan");
-emailinput.init();
+let email = new Validator("email", "emailspan", "email");
+email.init();
+
+let country = new Validator("country", "countryspan", "country");
+country.init();
+
+let zipcode = new Validator("zipcode", "zipcodespan", "zipcode");
+zipcode.init();
